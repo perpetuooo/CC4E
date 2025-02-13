670e1c39abdc45cc617027b15668902854794727
PH Perpétuo <pedrohenriqueperpetuo@gmail.com>
2025-02-13 17:45:31 -0300

    Update hex-converter.c

--- CC4E/Chapter-2/hex-converter.c
+++ CC4E/Chapter-2/hex-converter.c
@@ -5,9 +5,16 @@
 #define DIGITS "0123456789abcdef"
 #define MAX 30
 
-// void reverse(char r[]) {
+void reverse(char r[], int length) {
+    int i, j;
+    char temp;
 
-// }
+    for (i = 0, j = length - 1; i < j; i++, j--) {
+        temp = r[i];
+        r[i] = r[j];
+        r[j] = temp;
+    }
+}
 
 int main() {
     int x, y, length, digit, new;
@@ -29,10 +36,28 @@ int main() {
         length++;
     }
 
-    // reverse(out);
-    out[length + 1] = '\0';
+    out[length] = '\0'; 
+    reverse(out, length); 
 
-    printf("%d in base-8 is %s", x, out);
+    printf("%d in base-8 is %s\n", x, out);
+
+    // Base 16.
+    y = x;
+    length = 0;
+
+    while (y > 0) {
+        digit = y % 16;
+        c = DIGITS[digit];
+        out[length] = c;
+        new = y / 16;
+        y = new;
+        length++;
+    }
+
+    out[length] = '\0';
+    reverse(out, length);
+
+    printf("%d in base-16 is %s\n", x, out);
 
     return 0;
 }
